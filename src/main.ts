fb7c26f3e99348c0e93280fc31baf7400e2cec30
Atohallan <wu.jungmin0929@gmail.com>
2023-03-29 06:38:20 -0700

    Adding port

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: [process.env.CLIENT_ORIGIN],
     credentials: true,
   });
-  await app.listen(3000);
+
+  try {
+    await app.listen(process.env.PORT);
+    console.log(`Running on PORT ${process.env.PORT}.`);
+  } catch (err) {
+    console.log(err);
+  }
 }
 bootstrap();
