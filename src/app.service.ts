486fc2692f9f5f3e3056745c5a131a13bc7785eb
Atohallan <wu.jungmin0929@gmail.com>
2023-03-30 15:57:37 -0700

    Edited instructions for prompt

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,18 +8,22 @@ export class AppService {
   async createCompletion(userInput: string) {
     try {
       const instructions = `
-        A user has asked a question or asked for some sort of explanation.
-        You are having a conversation with another chat bot. 
-        You will expand upon things the other chat bot says to help the user with their question.
-        You can disagree, agree, and/or add alternatives if you wish.
-        The following question and chat history (if any) are as follows.
-        User input and chat history (if any) begins here:
-        ${userInput}
-        User input and chat history (if any) ended. Now you will respond.
-        If you just received a single question or request, then answer it normally.
-        Otherwise, respond to the last message in the chat history from the other bot.
-        If there is chat history, say things like "I agree" or "I disagree" so it appears as if you are having a conversation.
-        `;
+A user has asked a question or asked for some sort of explanation.
+You are having a conversation with another chat bot. 
+You will expand upon things the other chat bot says to help the user with their question.
+You can disagree, agree, and/or add alternatives if you wish.
+The following question and chat history (if any) are as follows.
+User input and chat history (if any) begins here:\n
+${userInput}
+User input and chat history (if any) ended. Now you will respond.
+If you just received a single question or request, then answer it normally.
+Otherwise, respond to the last message in the chat history from the other bot.
+If there is chat history, say things like "I agree" or "I disagree" so it appears as if you are having a conversation.
+Although, don't say "I agree" every time you respond. Try not to be repetitive with its usage.
+Think of other ways to make it appear like you are having a conversation.
+Also, don't label or reveal your response #. It may be confusing for the user.
+      `;
+      console.log('instructions:', instructions);
       const { data } = await this.openAIClient.createCompletion({
         model: 'text-davinci-003',
         max_tokens: 1000,
